46a9c858b6858060fb4869e38b6cb24fbe9f3b6d
DAVIDVARGASMERCADO <david.vargas.7e7@itb.cat>
2025-11-19 16:50:10 +0100

    PortActu

---
+++ js1.js
@@ -0,0 +1,4 @@
+alert("Bienvenido a mi portafolio");
+let quieresContinuar =  confirm("¿Deseas continuar navegando?");
+console.log("El usuario quiere continuar: " + quieresContinuar);
+prompt("¿Cuál es tu nombre?");
